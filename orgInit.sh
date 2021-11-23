dccb2e1c522891009116068729d65535fbcb5c1d
Jaswinder Rattanpal <jrattanpal@gmail.com>
2021-11-23 14:17:55 -0500

    initial sync

---
+++ orgInit.sh
@@ -0,0 +1,14 @@
+sfdx force:org:create -f config/project-scratch-def.json -a slackscratch1 -s -d 30 
+
+sfdx force:source:push
+
+sfdx force:user:permset:assign -n Slack
+
+sfdx force:org:open
+
+# sfdx force:user:password:generate --targetusername test-w76emxwtl7fz@example.com
+# Jbc6rq&iebpvv
+
+
+# sfdx force:user:display -u test-w76emxwtl7fz@example.com
+#https://flow-nosoftware-2952-dev-ed.cs69.my.salesforce.com
