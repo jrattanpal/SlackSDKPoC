79e4705c82ae7a3f0fdec8d4665c78dcd4894439
Jaswinder Rattanpal <jrattanpal@gmail.com>
2021-11-23 16:41:24 -0500

    update

--- orgInit.sh
+++ orgInit.sh
@@ -5,11 +5,25 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n Slack
 sfdx force:user:permset:assign -n Recruiting_Admin
 
+sfdx force:data:tree:import --plan data/slack-demo-plan.json
+
 sfdx force:org:open
 
-# sfdx force:user:password:generate --targetusername test-w76emxwtl7fz@example.com
-# pz!jiw4twSrvp
+# sfdx force:user:password:generate --targetusername test-zbvp2fwfgwmp@example.com
+# hY^7ijlhkmek
 
 
-# sfdx force:user:display -u test-w76emxwtl7fz@example.com
-#ttps://java-momentum-1541-dev-ed.cs96.my.salesforce.com
+# sfdx force:user:display -u test-zbvp2fwfgwmp@example.com
+# https://fun-page-8200-dev-ed.cs97.my.salesforce.com
+
+
+# sfdx force:data:tree:export --query "SELECT FIELDS(ALL) FROM Position__c LIMIT 200" --prefix slack-demo --outputdir data --plan
+# sfdx force:data:tree:export --query "SELECT FIELDS(ALL) FROM Candidate__c LIMIT 200" --prefix slack-demo --outputdir data --plan
+# sfdx force:data:tree:export --query "SELECT Interview_Date__c,Candidate__c,Interview_Type__c,Interviewer_Confirmed__c,Status__c FROM Interview__c LIMIT 200" --prefix slack-demo --outputdir data --plan
+
+
+
+Interview_Date__c": "2021-11-24T20:00:00.000+0000",
+            "Interview_Type__c": "Recruiter Interview",
+            "Interviewer_Confirmed__c": false,
+            "Status__c": "Pending"
